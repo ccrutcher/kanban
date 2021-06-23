5a3d673e801c565c4733a850ba59494cbb4e4da9
Cole <colejcrutcher@gmail.com>
2021-06-23 12:29:08 -0500

    Initial commit. Front end reaching back end.

---
+++ kanban-front/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <form action="../../post" method="post" className="form">
+        <button type="submit">Connected?</button>
+      </form>
+    </div>
+  )
+}
