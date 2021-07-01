d7afdd385663c40b90ed76e249b87af8c93579a9
Cole <colejcrutcher@gmail.com>
2021-07-01 12:18:36 -0500

    Added route for deleting a board

--- kanban-back/api/routes/board.js
+++ kanban-back/api/routes/board.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const boardController = require('../controllers/boardController');
 const listController = require('../controllers/listController');
   
-    // boardList Routes
+    // board routes
     router.get('/', boardController.list_all_boards);
     router.post('/', boardController.create_a_board);
+    router.delete('/:boardId', boardController.delete_a_board);
 
+    // list routes
     router.get('/:boardId', listController.list_board_lists);
     router.post('/:boardId', listController.create_a_list);
-  
-
+    
   
   module.exports = router;
